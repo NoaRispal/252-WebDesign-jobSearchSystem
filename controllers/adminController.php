f80aa1bfd9b4a8058dd75211119a7a967b173772
Noa Rispal <noa.rispal@grenoble-inp.org>
2026-04-16 21:14:48 +0700

    Feat : add controllers and models for employer and admin. Next

--- controllers/adminController.php
+++ controllers/adminController.php
@@ -4,4 +4,51 @@ main controller between the administrator interface and the database. Process al
 Allows admin to manage reference (lookup) tables
 And CRUD for job posting in sudo
 */
+include_once '../config/database.php';
+include_once '../models/jobVacancy.php';
+include_once '../models/referenceTables.php';
+
+class AdminController {
+    private $jobModel;
+    private $lookupModel;
+
+    public function __construct() {
+        $database = new Database();
+        $db = $database->getConnection();
+        $this->jobModel = new JobVacancy($db);
+        $this->lookupModel = new LookupModel($db);
+        
+        // Security : check if admin
+        if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') { 
+            header("Location: /login.php");
+            exit;
+        }
+    }
+
+    public function handleRequest() {
+        $action = $_GET['action'] ?? 'dashboard';
+
+        switch($action) {
+            case 'manage_jobs':
+                $jobs = $this->jobModel->readAll(); 
+                include '../views/admin/all_jobs.php';
+                break;
+
+            case 'delete_job':
+                $this->jobModel->delete($_GET['id']);
+                header("Location: ?action=manage_jobs");
+                break;
+
+            case 'manage_lookup':
+                $targetTable = $_GET['table'] ?? 'job_categories';
+                if ($_POST['new_name']) {
+                    $this->lookupModel->addEntry($targetTable, $_POST['new_name']);
+                }
+                $items = $this->lookupModel->getAll($targetTable);
+                include '../views/admin/lookup_list.php';
+                break;
+        }
+    }
+}
+
 ?>
--- controllers/jobController.php
+++ controllers/jobController.php
@@ -4,4 +4,46 @@ Employer Controller -> between the Employer UI and the JobVacancy model
 Handle CRUD 
 And toggling the status of their own job postings
 */
+include_once '../config/database.php';
+include_once '../models/jobVacancy.php';
+
+class JobController {
+    private $jobModel;
+
+    public function __construct() {
+        $database = new Database();
+        $db = $database->getConnection();
+        $this->jobModel = new JobVacancy($db);
+        
+        // Security : check if employer
+        if ($_SESSION['role'] !== 'employer') {
+            header("Location: /login.php");
+            exit;
+        }
+    }
+
+    public function listJobs() {
+        $employer_id = $_SESSION['user_id'];
+        return $this->jobModel->readByEmployer($employer_id);
+    }
+
+    public function handleRequest() {
+        $action = $_GET['action'] ?? 'list';
+
+        switch($action) {
+            case 'create':
+                if ($_POST) $this->jobModel->create($_POST);
+                include '../views/employer/create.php';
+                break;
+            case 'delete':
+                $this->jobModel->delete($_GET['id']);
+                header("Location: dashboard.php");
+                break;
+            case 'toggle':
+                $this->jobModel->toggleStatus($_GET['id'], $_GET['status']);
+                header("Location: dashboard.php");
+                break;
+        }
+    }
+}
 ?>
